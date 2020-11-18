ce45fc3b0828130c692b75a8297cbec11313c84d
Arne Hassel <arne.hassel@gmail.com>
2020-11-18 08:56:55 +0100

    Theme file cannot be tested it seems

--- src/theme/index.js
+++ src/theme/index.js
@@ -19,6 +19,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+/* istanbul ignore file */
 import { createTheme } from "@solid/lit-prism-patterns";
 import defaultTheme from "@solid/lit-prism-theme-sdk-default";
 
