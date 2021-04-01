c98a8fa14627e10c25b274aa9614a76f98567104
Virginia Balseiro <virginia.balseiro@inrupt.com>
2021-04-01 15:49:30 +0200

    Feat: displaying inherited access (#269)
    * Feat: displaying inherited access
    
    - add function to get inherited permissions
    
    - modify getPermissions functions so that they return all the permissions that apply to the resource, including inherited ones
    
    - add tests
    
    * switching filtering from hardcoded string to policies container url
    
    * using type map instead of hardcoding strings

--- components/resourceDetails/resourceSharing/agentAccessTable/index.jsx
+++ components/resourceDetails/resourceSharing/agentAccessTable/index.jsx
@@ -56,14 +56,14 @@ export default function AgentAccessTable({ type }) {
   const [loading, setLoading] = useState(false);
   const [permissions, setPermissions] = useState([]);
   const {
-    data: namedPermissions,
+    data: policyPermissions,
     mutate: mutatePermissions,
   } = usePolicyPermissions(type);
 
   useEffect(() => {
-    if (!namedPermissions) return;
-    setPermissions(namedPermissions);
-  }, [namedPermissions]);
+    if (!policyPermissions) return;
+    setPermissions(policyPermissions);
+  }, [policyPermissions]);
 
   const bem = useBem(useStyles());
 
