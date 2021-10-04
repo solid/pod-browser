975bcaf2a801f392105f6863583ae7fb80e0d029
Virginia Balseiro <virginia.balseiro@inrupt.com>
2021-10-04 13:41:25 +0200

    upgrading to latest solid-client and solid-client-consent - fixing tests

--- components/pages/privacy/consent/requests/index.jsx
+++ components/pages/privacy/consent/requests/index.jsx
@@ -56,6 +56,7 @@ export default function ConsentShow() {
   const agentWebId = getRequestorWebId(consentRequest);
   const [agentDetails, setAgentDetails] = useState({});
   const { agentName, agentUrl, agentPolicy, agentTOS } = agentDetails || null;
+
   useEffect(() => {
     if (!requestVc) return;
     const req = JSON.parse(atob(requestVc));
