5526b23a4a61fb04db9c9498337094a010a197b1
Norbert Nagy <norbertnadj@gmail.com>
2021-10-21 14:19:52 +0100

    rebasing 1005 branch

--- components/resourceDetails/resourceSharing/agentAccess/agentProfileDetails/index.jsx
+++ components/resourceDetails/resourceSharing/agentAccess/agentProfileDetails/index.jsx
@@ -99,7 +99,7 @@ export default function AgentProfileDetails({
       >
         {localProfile ? displayProfileName(localProfile) : webId}
       </Typography>
-      {!permission.inherited && !permission.vc ? (
+      {!permission.inherited ? (
         <AgentAccessOptionsMenu
           resourceIri={resourceIri}
           profile={profile}
