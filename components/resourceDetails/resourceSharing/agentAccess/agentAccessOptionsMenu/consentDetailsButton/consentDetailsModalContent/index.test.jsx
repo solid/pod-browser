ac5445b7100459124de38c0bb96c5e22c6465ba9
Norbert Nagy <norbertnadj@gmail.com>
2021-10-25 10:02:12 +0100

    Added test for consent modal details content component

---
+++ components/resourceDetails/resourceSharing/agentAccess/agentAccessOptionsMenu/consentDetailsButton/consentDetailsModalContent/index.test.jsx
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import React from "react";
+import * as solidClientFns from "@inrupt/solid-client";
+import { renderWithTheme } from "../../../../../../../__testUtils/withTheme";
+import {
+  aliceWebIdUrl,
+  mockPersonDatasetAlice,
+} from "../../../../../../../__testUtils/mockPersonResource";
+import getSignedVc from "../../../../../../../__testUtils/mockSignedVc";
+import ConsentDetailsModalContent, {
+  TESTCAFE_ID_CONSENT_DETAILS_CONTENT,
+} from "./index";
+
+const webId = "https://example.com/profile/card#me";
+
+const permission = {
+  webId,
+  alias: "Editors",
+  type: "agent",
+  vc: getSignedVc(),
+};
+
+describe("Renders correct consent modal content", () => {
+  const profileDataset = mockPersonDatasetAlice();
+  const profileThing = solidClientFns.getThing(profileDataset, aliceWebIdUrl);
+  beforeEach(() => {
+    jest
+      .spyOn(solidClientFns, "getSolidDataset")
+      .mockResolvedValue(profileDataset);
+    jest.spyOn(solidClientFns, "getThing").mockReturnValue(profileThing);
+    jest.spyOn(solidClientFns, "getUrl").mockReturnValue("schema.Person");
+  });
+
+  test("clicking on view details button renders a confirmation dialog with the correct data", async () => {
+    const { baseElement, findByTestId, findByText } = renderWithTheme(
+      <ConsentDetailsModalContent permission={permission} />
+    );
+    await findByTestId(TESTCAFE_ID_CONSENT_DETAILS_CONTENT);
+    await findByText(
+      permission.vc.credentialSubject.providedConsent.forPurpose[0]
+    );
+    expect(baseElement).toMatchSnapshot();
+  });
+});
