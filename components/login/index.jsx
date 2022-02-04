7acb93e33b517f5618b0090e603e66986f34e0cd
Anna Kallenborn-Bolden <94387648+solid-akb@users.noreply.github.com>
2022-02-04 14:45:00 +0100

    updated client_id logic (#422)
    * updated client_id logic

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -62,8 +62,8 @@ export default function Login() {
 
   const authOptions = {
     clientName: CLIENT_NAME,
-    clientId: oidcSupported ? CLIENT_APP_WEBID : null,
   };
+  if (oidcSupported) authOptions.clientId = CLIENT_APP_WEBID;
 
   useEffect(() => {
     if (!idp) return;
