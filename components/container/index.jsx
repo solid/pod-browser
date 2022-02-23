2ef9a08c9c6baca1b2317c59328bc9435ec028ff
Dayton Nolan <me@daytonnolan.com>
2022-02-23 15:40:13 -0700

    Minor cleanup

--- components/container/index.jsx
+++ components/container/index.jsx
@@ -49,6 +49,32 @@ import { isHTTPError } from "../../src/error";
 import { locationIsConnectedToProfile } from "../../src/solidClientHelpers/profile";
 import { isContainerIri } from "../../src/solidClientHelpers/utils";
 
+function isNotAContainerResource(iri, container) {
+  if (!iri) return true;
+  return (
+    container && isContainerIri(iri) && getSourceUrl(container.dataset) !== iri
+  );
+}
+
+function renderContainerContent(isValidating, iri, data) {
+  return isValidating ? (
+    <Spinner />
+  ) : (
+    <ContainerTable
+      containerPath={getContainerUrl(iri)}
+      data={data}
+      resourcePath={iri}
+    />
+  );
+}
+
+function maybeRenderWarning(locationIsInUsersPod, noControlError, podRootIri) {
+  return (
+    locationIsInUsersPod &&
+    noControlError && <NoControlWarning podRootIri={podRootIri} />
+  );
+}
+
 export default function Container({ iri }) {
   useRedirectIfLoggedOut();
   const { sessionRequestInProgress } = useSession();
@@ -68,13 +94,7 @@ export default function Container({ iri }) {
   } = useContainer(iri);
 
   useEffect(() => {
-    if (
-      !iri ||
-      (container &&
-        isContainerIri(iri) &&
-        getSourceUrl(container.dataset) !== iri)
-    )
-      return;
+    if (isNotAContainerResource(iri, container)) return;
     const urls = container && getContainerResourceUrlAll(container);
     setResourceUrls(urls);
   }, [container, iri]);
@@ -127,18 +147,8 @@ export default function Container({ iri }) {
         <PageHeader />
         <ContainerDetails update={update}>
           <ContainerSubHeader update={update} resourceList={data} />
-          {locationIsInUsersPod && noControlError && (
-            <NoControlWarning podRootIri={podRootIri} />
-          )}
-          {isValidating ? (
-            <Spinner />
-          ) : (
-            <ContainerTable
-              containerPath={getContainerUrl(iri)}
-              data={data}
-              resourcePath={iri}
-            />
-          )}
+          {maybeRenderWarning(locationIsInUsersPod, noControlError, podRootIri)}
+          {renderContainerContent(isValidating, iri, data)}
         </ContainerDetails>
       </BookmarksContextProvider>
     </>
