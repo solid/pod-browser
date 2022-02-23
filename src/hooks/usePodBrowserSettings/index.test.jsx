3e976141a3afb0783a93a8d31b9ae76be64aaf6f
Dayton Nolan <me@daytonnolan.com>
2022-02-23 09:00:36 -0700

    Change tests to accomodate new profile workflow

--- src/hooks/usePodBrowserSettings/index.test.jsx
+++ src/hooks/usePodBrowserSettings/index.test.jsx
@@ -23,6 +23,7 @@ import * as solidClientFns from "@inrupt/solid-client";
 import { renderHook, waitFor } from "@testing-library/react-hooks";
 import mockSession, {
   mockUnauthenticatedSession,
+  newProfileTurtle,
   profileTurtle,
   webIdUrl,
 } from "../../../__testUtils/mockSession";
